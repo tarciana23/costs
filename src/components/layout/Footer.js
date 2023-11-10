99b88159ec012bf19313277013f11bd97b318b61
Tarciana Oliveira <tarciana.trabalhos@outlook.com>
2023-11-10 14:45:31 -0300

    Home feita

---
+++ src/components/layout/Footer.js
@@ -0,0 +1,23 @@
+import {FaFacebook, FaInstagram, FaLinkedin} from 'react-icons/fa'
+
+import styles from './Footer.module.css'
+
+function Footer (){
+    return (
+    <footer className={styles.footer}>
+            <ul className={styles.list}>
+                <li className={styles.item}><FaFacebook/></li>
+                <li className={styles.item}><FaInstagram/></li>
+                <li className={styles.item}><FaLinkedin/></li>
+               </ul>
+
+            <p className={styles.copy_right}>
+                <span>Costs</span> &copy; 2023
+                </p>
+        </footer>
+    
+    
+    )
+}
+   
+export default Footer
