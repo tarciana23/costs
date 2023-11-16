b38da939214cba6bbff91a1e7c3b3c16b9adbe6f
Tarciana Oliveira <anarumiroan@gmail.com>
2023-11-16 09:59:08 -0300

    teste

--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -33,7 +33,7 @@ function ProjectForm ({handleSubmit,btnText,projectData}){
     return (
         <form onSubmit={submit} className={styles.form}>
             <Input 
-                type="text"
+                type="number"
                 text="Nome do projeto"
                 name="name"
                 placeholder="Insira o nome do projeto"
